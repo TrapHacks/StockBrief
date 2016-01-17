b07cba11963d0ac6d94a0fabb1e94b01d81a5e34
Mohammad Anees <mohammad.irock@gmail.com>
2016-01-17 00:12:24 -0600

    fix post issue

--- static/js/stock-chart.js
+++ static/js/stock-chart.js
@@ -5,7 +5,8 @@ var lineChartData = {}
 $("#search").click(function() {
   //post request that gets data 
   var search_val = $('#stock_name').val();
-  var return_data = $.post("/search",search_val,function(data) {
+  console.log({"search_val": search_val});
+  var return_data = $.post("/search",{"search_val": search_val},function(data) {
     lineChartData= {
         labels : data.dates.slice(0,50).slice(4),
         datasets : [
