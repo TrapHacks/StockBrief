69eda132e6d53ecc3bdca80e9ff639cf89c877c8
Daniel Dao <daniel.vu.dao@gmail.com>
2016-01-17 03:55:10 -0600

    debug

--- stockbrief.py
+++ stockbrief.py
@@ -101,7 +101,7 @@ def not_found(error):
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
 
-	if port == 5000:
-		app.debug = True
+	# if port == 5000:
+	app.debug = True
 
 	app.run()
