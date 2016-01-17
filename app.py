4a90f28f7668e7f9206dd9a38ee39e90b11b5ef4
walidowais <walidowais@gmail.com>
2016-01-16 23:05:25 -0600

    fake data real app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import csv
 import os
 
@@ -10,11 +10,20 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route("/")
-def fake_data():
-	with open('stock_data/MMM.csv', 'rb') as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=',', quitechar='|')
-	return "fuck off"
+@app.route("/search", methods=['POST', 'GET'])
+def search():
+	date_list = []
+	price_list = []
+
+	with open('test_data.csv', 'rb') as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+		next(csv_reader, None)
+
+		for row in csv_reader:
+			date_list.append(row[5])
+			price_list.append(row[6])
+
+	return jsonify(dates=date_list, prices=price_list)
 
 @app.errorhandler(404)
 def not_found(error):
