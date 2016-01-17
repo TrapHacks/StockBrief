6d19b0b2dcd5a42f355c30347eb10ffd21405811
walidowais <walidowais@gmail.com>
2016-01-16 22:55:03 -0600

    move js

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template
+import csv
 import os
 
 app = Flask(__name__)
@@ -8,6 +9,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	return render_template('index.html')
+
+@app.route("/")
+def fake_data():
+	with open('stock_data/MMM.csv', 'rb') as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',', quitechar='|')
+	return "fuck off"
 
 @app.errorhandler(404)
 def not_found(error):
